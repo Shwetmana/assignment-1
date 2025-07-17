82c8ee31ab654c5deb120abbb27da718331ecf3f
Shwetmana <shwetmanaroy@gmail.com>
2025-07-18 04:08:30 +0530

    Add files via upload

---
+++ assignmant 1.py
@@ -0,0 +1,19 @@
+#Task- perform basic mathematical operations
+a=input("enter the first number: ")
+b=input("enter the second number: ")
+a=int(a)
+b=int(b)
+add=a+b
+sub=a-b
+mult=a*b
+div=a/b
+print("addition: ",add)
+print("subtraction: ",sub)
+print("multiplication: ",mult)
+print("division: ",div)
+
+#Task- create a personalized greeting
+a=input("enter your first name: ")
+b=input("enter your second name: ")
+fullname=a+b
+print('Hello,' ,fullname, 'welcome to the python program')
